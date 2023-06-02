34b44251017cdbcbe0d8036ef5aa7698e4b99137
le-nate <99023298+le-nate@users.noreply.github.com>
2023-06-02 16:23:27 +0200

    add: no click zone and labels

--- flocking.js
+++ flocking.js
@@ -5,43 +5,46 @@ let cnvWidth;
 let cnvHeight;
 let cnvX;
 let cnvY;
+let sliderX;
 
 let perceptionSlider, alignSlider, cohesionSlider, separationSlider;
 let maxForceSlider, maxSpeedSlider;
 
 function setup() {
-    cnvWidth = windowWidth * 0.6;
+    cnvWidth = windowWidth * 0.95;
     cnvHeight = windowHeight;
     cnvX = 0;
-    cnvY = windowHeight * .15;
+    cnvY = 0;
+    sliderX = windowWidth * 0;
+    sliderSeparation = .04;
 
     let cnv = createCanvas(cnvWidth, cnvHeight);
     cnv.position(cnvX, cnvY)
     resetSketch();
     console.log(startTime);
 
-    button = createButton('stop');
-    button.position(windowWidth * .2, windowHeight * .13);
-    button.mousePressed(stopFlock);
-    button = createButton('start');
-    button.position(windowWidth * .25, windowHeight * .13);
-    button.mousePressed(startFlock);
-    button = createButton('reset');
-    button.position(windowWidth * .3, windowHeight * .13);
-    button.mousePressed(resetSketch);
-
     maxForceSlider = createSlider(0, 5, 1.34, .02);
-    maxForceSlider.position(windowWidth * 0, windowHeight * 0.03)
+    maxForceSlider.position(sliderX, windowHeight * sliderSeparation * 1);
     maxSpeedSlider = createSlider(0, 100, 5, 5);
-    maxSpeedSlider.position(windowWidth * 0, windowHeight * 0.05)
+    maxSpeedSlider.position(sliderX, windowHeight * sliderSeparation * 2)
     perceptionSlider = createSlider(0, 100, 90, 10);
-    perceptionSlider.position(windowWidth * 0, windowHeight * 0.07)
+    perceptionSlider.position(sliderX, windowHeight * sliderSeparation * 3)
     alignSlider = createSlider(0, 10, 8, .25);
-    alignSlider.position(windowWidth * 0, windowHeight * 0.09)
+    alignSlider.position(sliderX, windowHeight * sliderSeparation * 4)
     cohesionSlider = createSlider(0, 10, 2, .25);
-    cohesionSlider.position(windowWidth * 0, windowHeight * 0.11)
+    cohesionSlider.position(sliderX, windowHeight * sliderSeparation * 5)
     separationSlider = createSlider(0, 5, 2.5, .5);
-    separationSlider.position(windowWidth * 0, windowHeight * 0.13)
+    separationSlider.position(sliderX, windowHeight * sliderSeparation * 6)
+
+    buttonStop = createButton('stop');
+    buttonStop.position(windowWidth * .2, maxForceSlider.y);
+    buttonStop.mousePressed(stopFlock);
+    buttonStart = createButton('start');
+    buttonStart.position(windowWidth * .25, maxForceSlider.y);
+    buttonStart.mousePressed(startFlock);
+    buttonReset = createButton('reset');
+    buttonReset.position(windowWidth * .3, maxForceSlider.y);
+    buttonReset.mousePressed(resetSketch);
 
     // // pre-determine number of boids
     // Generate initial flock
@@ -76,6 +79,14 @@ function draw() {
 
     // document.getElementById(`boid.velocity`).innerHTML = boid.velocity.mag();
     // document.getElementById(`boid.history`).innerHTML = boid.history.length;
+
+    // text(`Max force: ${maxForceSlider.value()}`, maxForceSlider.x * 2 + maxForceSlider.width, maxForceSliderY);
+    text(`Max force: ${maxForceSlider.value()}`, maxForceSlider.x * 2 + maxForceSlider.width, maxForceSlider.y + 5);
+    text(`Max speed: ${maxSpeedSlider.value()}`, maxSpeedSlider.x * 2 + maxSpeedSlider.width, maxSpeedSlider.y + 5);
+    text(`Perception radius: ${perceptionSlider.value()}`, perceptionSlider.x * 2 + perceptionSlider.width, perceptionSlider.y + 5);
+    text(`Alignment: ${alignSlider.value()}`, alignSlider.x * 2 + alignSlider.width, alignSlider.y + 5);
+    text(`Cohesion: ${cohesionSlider.value()}`, cohesionSlider.x * 2 + cohesionSlider.width, cohesionSlider.y + 5);
+    text(`Separation: ${separationSlider.value()}`, separationSlider.x * 2 + separationSlider.width, separationSlider.y + 5);
     document.getElementById('frc').innerHTML = maxForceSlider.value();
     document.getElementById('spd').innerHTML = maxSpeedSlider.value();
     document.getElementById('per').innerHTML = perceptionSlider.value();
@@ -93,12 +104,25 @@ function startFlock() {
 }
 
 function mousePressed() {
-    if (mouseX < cnvWidth && cnvY < mouseY) {
-        console.log('(', cnvX, cnvWidth, ', ', cnvY, cnvHeight, ')');
+    if (maxForceSlider.x * 2 + maxForceSlider.width < mouseX && mouseY > separationSlider.y) {
+        console.log('1st (', maxForceSlider, ', ', separationSlider.y, ')');
         console.log(mouseX, mouseY);
         for (let i = 0; i < 5; i++) {
             flock.push(new Boid());
         };
+    } else if (mouseX > buttonReset.x * 1.2) {
+        console.log('2n')
+        for (let i = 0; i < 5; i++) {
+            flock.push(new Boid());
+        };
+    } else if (mouseX > maxForceSlider.x * 2 + maxForceSlider.width && mouseX < buttonReset.x * 2 && mouseY > buttonReset.y * 2) {
+        console.log('yes', buttonReset.y, mouseY);
+
+        for (let i = 0; i < 5; i++) {
+            flock.push(new Boid());
+        };
+    } else {
+        console.log(buttonReset.x, buttonReset.y, mouseX, mouseY);
     }
 }
 
